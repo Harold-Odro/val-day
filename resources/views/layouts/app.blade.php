4b3599ef31f7359be0a56908b539b59c68e5d793
Harold Odro <harodro@gmail.com>
2025-02-12 10:45:51 +0000

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,36 +3,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Valentine's Day</title>
-
     <!-- Styles -->
     @if (app()->environment('production'))
-        <link rel="stylesheet" href="{{ Vite::asset('resources/css/app.css') }}">
+        @vite(['resources/css/app.css'])
     @else
         @vite(['resources/css/app.css'])
     @endif
-
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-
     <!-- Scripts -->
     <script defer src="https://unpkg.com/alpinejs@3.10.5/dist/cdn.min.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/intersect@3.x.x/dist/cdn.min.js"></script>
     <script defer src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-
     <script>
         document.addEventListener('alpine:init', () => {
             Alpine.plugin(Intersect);
         });
-
         document.addEventListener('DOMContentLoaded', function() {
             if (typeof Alpine === 'undefined') console.error('Alpine.js not loaded');
             if (typeof Swiper === 'undefined') console.error('Swiper not loaded');
             if (typeof confetti === 'undefined') console.error('Confetti not loaded');
         });
     </script>
-
     @if (app()->environment('production'))
-        <script defer src="{{ Vite::asset('resources/js/app.js') }}"></script>
+        @vite(['resources/js/app.js'])
     @else
         @vite(['resources/js/app.js'])
     @endif
