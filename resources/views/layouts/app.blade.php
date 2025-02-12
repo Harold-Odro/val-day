6c911e146c9352630cbd5e9a6fa1c8230af7f461
Harold Odro <harodro@gmail.com>
2025-02-12 06:55:33 +0000

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,17 +3,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Valentine's Day</title>
-    
+
     <!-- Styles -->
-    @vite(['resources/css/app.css'])
+    @if (app()->environment('production'))
+        <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
+    @else
+        @vite(['resources/css/app.css'])
+    @endif
+
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-    
+
     <!-- Scripts -->
     <script defer src="https://unpkg.com/alpinejs@3.10.5/dist/cdn.min.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/intersect@3.x.x/dist/cdn.min.js"></script>
-    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    
+    <script defer src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+
     <script>
         document.addEventListener('alpine:init', () => {
             Alpine.plugin(Intersect);
@@ -26,5 +31,9 @@
         });
     </script>
 
-    @vite(['resources/js/app.js'])
+    @if (app()->environment('production'))
+        <script defer src="{{ asset('build/assets/app.js') }}"></script>
+    @else
+        @vite(['resources/js/app.js'])
+    @endif
 </head>
