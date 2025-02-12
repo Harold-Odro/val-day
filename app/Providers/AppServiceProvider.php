dfb1132d51cc9ccf9880a489d4a37aaa82a60edc
Harold Odro <harodro@gmail.com>
2025-02-12 06:25:40 +0000

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,23 +1,33 @@
-   <?php
+<?php
 
-   namespace App\Providers;
+namespace App\Providers;
 
-   use Illuminate\Support\ServiceProvider;
-   use Illuminate\Pagination\Paginator;
-   use Illuminate\Support\Facades\Blade;
-   use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 
-   class AppServiceProvider extends ServiceProvider
-   {
-       /**
-        * Register any application services.
-        *
-        * @return void
-        */
-       public function register()
-       {
-           if (env('APP_ENV') !== 'local') {
-               URL::forceScheme('https');
-           }
-       }
-   }
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Your boot method code here
+    }
+}
